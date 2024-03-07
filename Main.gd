7f05fdb4d0be72890c46be95a59c09c76dee74ed
Ashton Meuser <ashtonmeuser@gmail.com>
2024-03-06 18:16:09 -0800

    Additional key bindings
    Alpha, numeric, & function keys

--- Main.gd
+++ Main.gd
@@ -4,9 +4,10 @@ extends Control
 @onready var memory = WasmMemory.new()
 var texture = ImageTexture.new()
 var image = Image.new()
-var keys = { KEY_ENTER: 13, KEY_BACKSPACE: 127, KEY_SPACE: 32, KEY_LEFT: 0xac, KEY_RIGHT: 0xae, KEY_UP: 0xad, KEY_DOWN: 0xaf, KEY_CTRL: 0x80+0x1d, KEY_ALT: 0x80+0x38, KEY_ESCAPE: 27, KEY_TAB: 9, KEY_SHIFT: 16, KEY_Y: KEY_Y + 32 }
+var keys = { KEY_ENTER: 13, KEY_BACKSPACE: 127, KEY_SPACE: 32, KEY_LEFT: 0xac, KEY_RIGHT: 0xae, KEY_UP: 0xad, KEY_DOWN: 0xaf, KEY_CTRL: 0x80+0x1d, KEY_ALT: 0x80+0x38, KEY_ESCAPE: 27, KEY_TAB: 9, KEY_SHIFT: 16 }
 
 func _ready():
+	for k in range(KEY_A, KEY_Z + 1).map(func(x): return { x: x + 32 }) + range(KEY_0, KEY_9 + 1).map(func(x): return { x: x }) + range(KEY_F1, KEY_F12 + 1).map(func(x): return { x: 187 + x - KEY_F1 }): keys.merge(k)
 	$TextureRect.texture = texture
 	image = Image.create(640, 400, false, Image.FORMAT_RGBA8)
 	texture.set_image(image)
