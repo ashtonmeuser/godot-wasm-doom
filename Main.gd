e6c580e77950cb3a3cfdbe44924f9e51d920fdff
Ashton Meuser <ashtonmeuser@gmail.com>
2024-03-06 20:30:59 -0800

    TextureRect defined in scene, ignore echo events

--- Main.gd
+++ Main.gd
@@ -2,15 +2,13 @@ extends Control
 
 @onready var wasm = Wasm.new()
 @onready var memory = WasmMemory.new()
-var texture = ImageTexture.new()
 var image = Image.new()
 var keys = { KEY_ENTER: 13, KEY_BACKSPACE: 127, KEY_SPACE: 32, KEY_LEFT: 0xac, KEY_RIGHT: 0xae, KEY_UP: 0xad, KEY_DOWN: 0xaf, KEY_CTRL: 0x80+0x1d, KEY_ALT: 0x80+0x38, KEY_ESCAPE: 27, KEY_TAB: 9, KEY_SHIFT: 16 }
 
 func _ready():
 	for k in range(KEY_A, KEY_Z + 1).map(func(x): return { x: x + 32 }) + range(KEY_0, KEY_9 + 1).map(func(x): return { x: x }) + range(KEY_F1, KEY_F12 + 1).map(func(x): return { x: 187 + x - KEY_F1 }): keys.merge(k)
-	$TextureRect.texture = texture
 	image = Image.create(640, 400, false, Image.FORMAT_RGBA8)
-	texture.set_image(image)
+	$TextureRect.texture.set_image(image)
 	memory.grow(108)
 	var imports = {
 		"functions": { "js.js_console_log": [self, "stdout"], "js.js_draw_screen": [self, "draw_screen"], "js.js_milliseconds_since_start": [Time, "get_ticks_msec"], "js.js_stdout": [self, "stdout"], "js.js_stderr": [self, "stderr"] },
@@ -23,11 +21,11 @@ func _process(_delta):
 	wasm.function("doom_loop_step", [])
 
 func _input(event):
-	if event is InputEventKey: wasm.function("add_browser_event", [int(!event.is_pressed()), keys.get(event.keycode, 0)])
+	if event is InputEventKey and !event.is_echo(): wasm.function("add_browser_event", [int(!event.is_pressed()), keys.get(event.keycode, 0)])
 
 func draw_screen(offset):
 	image.set_data(640, 400, false, Image.FORMAT_RGBA8, memory.seek(offset).get_data(640 * 400 * 4)[1])
-	texture.update(image)
+	$TextureRect.texture.update(image)
 
 func stdout(offset, length):
 	print(memory.seek(offset).get_utf8_string(length))
